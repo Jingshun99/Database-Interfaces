3dc40cd81d4a7f8c9403fcc6602718b0bc8a8273
Jingshun99 <63948056+Jingshun99@users.noreply.github.com>
2020-04-25 12:54:10 +0800

    Update table3.php

--- table3.php
+++ table3.php
@@ -3,31 +3,31 @@
     include 'cwindex.php';
 
     if ($_GET['submit']=='INSERT'){ //if we pressed insert
-        echo "Enter information:";
-        echo "<br>";
+        echo "<p> <font color=white>Enter information:";
+        //echo "<br>";
         //echo "<i>For inserting a text, please enclosed text with quotation(' ')</i>";
         echo "<br>";
         echo "<br>";
         echo "<form action = 'insert3.php' method = 'get'>";
         if(!empty($_GET["col3"])){
             foreach($_GET["col3"] as $col3){
-                echo "$col3<input type = 'checkbox', value = '$col3', name = 'col3[]' checked> : <input type = 'text' name = 'inscol3[]'>";
+                echo "<p> <font color=white>$col3<input type = 'checkbox', value = '$col3', name = 'col3[]' checked> : <input type = 'text' name = 'inscol3[]'>";
                 echo "<br>";
                 echo "<br>";
             }
-            echo "<input type = 'submit' name = 'insert' value = 'submit'>";
+            echo "<input type = 'submit' name = 'insert' value = 'submit' id='submit1_btn' class='submit1'>";
         }
 
         echo "</form>";
         echo "<br>";
-    echo "<table border='1' cellpadding='2'>";
+    echo "<table border='1' cellpadding='2' bordercolor='#fff200'>";
     echo "<tr>";
     if(isset($_GET['submit'])){
         if(!empty($_GET["col3"])){
             foreach($_GET["col3"] as $col3){
-                echo"<th>$col3</th>";
+                echo"<th><p> <font color=white>$col3</th>";
             }
-            echo"<th>ACTIONS</th>";
+            echo"<th><p> <font color=white>ACTIONS</th>";
         echo"</tr>";
 
 		$sql = "SELECT * FROM lang;";
@@ -36,30 +36,29 @@
 			while($row = mysqli_fetch_assoc($result)){
                 echo "<tr>";
                 foreach($_GET["col3"] as $col3){
-                    echo "<td>".$row["$col3"]."</td>";
+                    echo "<td><p> <font color=white>".$row["$col3"]."</td>";
                 }
-                echo "<td><input type = 'submit' value = 'UPDATE'> <input type = 'submit' value = 'DELETE'></td>";
+                echo "<td><input type = 'submit' value = 'UPDATE' id='update_btn' class= 'update'> <input type = 'submit' value = 'DELETE' id='delete_btn' class= 'delete'></td>";
                 echo "</tr>";
             }
         }
         else{
-            echo "Please select at least one column";
+            echo "<p> <font color=white>Please select at least one column";
         }
     }
         echo "</table>";
     }
 
     else{ //if we pressed select
-    echo "<b><ul><u>language</b></ul></u>";
     echo "<br>";
-    echo "<table border='1' cellpadding='2'>";
+    echo "<table border='1' cellpadding='2' bordercolor='#fff200'>";
     echo "<tr>";
     if(isset($_GET['submit'])){
         if(!empty($_GET["col3"])){
             foreach($_GET["col3"] as $col3){
-                echo"<th>$col3</th>";
+                echo"<th><p> <font color=white>$col3</th>";
             }
-            echo"<th>ACTIONS</th>";
+            echo"<th><p> <font color=white>ACTIONS</th>";
         echo"</tr>";
 
 		$sql = "SELECT * FROM lang;";
@@ -68,14 +67,14 @@
 			while($row = mysqli_fetch_assoc($result)){
                 echo "<tr>";
                 foreach($_GET["col3"] as $col3){
-                    echo "<td>".$row["$col3"]."</td>";
+                    echo "<td><p> <font color=white>".$row["$col3"]."</td>";
                 }
-                echo "<td><input type = 'submit' value = 'UPDATE'> <input type = 'submit' value = 'DELETE'></td>";
+                echo "<td><input type = 'submit' value = 'UPDATE' id='update_btn' class= 'update'> <input type = 'submit' value = 'DELETE' id='delete_btn' class= 'delete'></td>";
                 echo "</tr>";
             }
         }
         else{
-            echo "Please select at least one column";
+            echo "<p> <font color=white>Please select at least one column";
         }
     }
         echo "</table>";
