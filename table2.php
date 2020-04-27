6eb37d7eea5c801f539aaf4b9c8d5dfbb471fea6
Jingshun99 <63948056+Jingshun99@users.noreply.github.com>
2020-04-27 12:45:55 +0800

    Add files via upload

--- table2.php
+++ table2.php
@@ -1,33 +1,34 @@
+
 <?php
     include_once 'demos/demo1.php';
     include 'cwindex.php';
 
     if ($_GET['submit']=='INSERT'){ //if we pressed insert
-        echo "Enter information:";
-        echo "<br>";
+        echo "<p> <font color=white>Enter information:";
+        //echo "<br>";
         //echo "<i>For inserting a text, please enclosed text with quotation(' ')</i>";
         echo "<br>";
         echo "<br>";
         echo "<form action = 'insert2.php' method = 'get'>";
         if(!empty($_GET["col2"])){
             foreach($_GET["col2"] as $col2){
-                echo "$col2<input type = 'checkbox', value = '$col2', name = 'col2[]' checked> : <input type = 'text' name = 'inscol2[]'>";
+                echo "<p> <font color=white>$col2<input type = 'checkbox', value = '$col2', name = 'col2[]' checked> : <input type = 'text' name = 'inscol2[]' id='datacol' class='data'>";
                 echo "<br>";
                 echo "<br>";
             }
-            echo "<input type = 'submit' name = 'insert' value = 'submit'>";
+            echo "<input type = 'submit' name = 'insert' value = 'submit' id='submit1_btn' class='submit1'>";
         }
 
         echo "</form>";
         echo "<br>";
-    echo "<table border='1' cellpadding='2'>";
+    echo "<table border='1' cellpadding='2' bordercolor='#f0932b'>";
     echo "<tr>";
     if(isset($_GET['submit'])){
         if(!empty($_GET["col2"])){
             foreach($_GET["col2"] as $col2){
-                echo"<th>$col2</th>";
+                echo"<th><p> <font color=white>$col2</th>";
             }
-            echo"<th>ACTIONS</th>";
+            echo"<th><p> <font color=white>ACTIONS</th>";
         echo"</tr>";
 
 		$sql = "SELECT * FROM country;";
@@ -36,30 +37,29 @@
 			while($row = mysqli_fetch_assoc($result)){
                 echo "<tr>";
                 foreach($_GET["col2"] as $col2){
-                    echo "<td>".$row["$col2"]."</td>";
+                    echo "<td><p> <font color=white>".$row["$col2"]."</td>";
                 }
-                echo "<td><input type = 'submit' value = 'UPDATE'> <input type = 'submit' value = 'DELETE'></td>";
+                echo "<td><input type = 'submit' value = 'UPDATE' id='update_btn' class= 'update'> <input type = 'submit' value = 'DELETE' id='delete_btn' class= 'delete'></td>";
                 echo "</tr>";
             }
         }
         else{
-            echo "Please select at least one column";
+            echo "<p> <font color=white>Please select at least one column";
         }
     }
         echo "</table>";
     }
 
     else{ //if we pressed select
-    echo "<b><ul><u>country</b></ul></u>";
     echo "<br>";
-    echo "<table border='1' cellpadding='2'>";
+    echo "<table border='1' cellpadding='2' bordercolor='#f0932b'>";
     echo "<tr>";
     if(isset($_GET['submit'])){
         if(!empty($_GET["col2"])){
             foreach($_GET["col2"] as $col2){
-                echo"<th>$col2</th>";
+                echo"<th><p> <font color=white>$col2</th>";
             }
-            echo"<th>ACTIONS</th>";
+            echo"<th><p> <font color=white>ACTIONS</th>";
         echo"</tr>";
 
 		$sql = "SELECT * FROM country;";
@@ -68,21 +68,16 @@
 			while($row = mysqli_fetch_assoc($result)){
                 echo "<tr>";
                 foreach($_GET["col2"] as $col2){
-                    echo "<td>".$row["$col2"]."</td>";
+                    echo "<td><p> <font color=white>".$row["$col2"]."</td>";
                 }
-
-                //echo "<td><input type = 'submit' value = 'UPDATE'> <input type = 'submit' value = 'DELETE'></td>";
-                echo '<td><form action="delete2.php" method="get"> <input type="hidden" name="primKey" value='.$row["Country_abb"].'> <input type="submit" name="delete" value="DELETE"></form></td>';
-                //echo "<td>".$row["Country_abb"]."</td>";
-
+                echo "<td><input type = 'submit' value = 'UPDATE' id='update_btn' class= 'update'> <input type = 'submit' value = 'DELETE' id='delete_btn' class= 'delete'></td>";
                 echo "</tr>";
             }
         }
         else{
-            echo "Please select at least one column";
+            echo "<p> <font color=white>Please select at least one column";
         }
     }
         echo "</table>";
 }
-
 ?>
